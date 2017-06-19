f1132111003068273cd648a99eacf750fb2136df
Alper Keskin <alper.keskin@argedor.com>
2017-06-19 16:45:22 +0300

    Dummy MeshUtils.cs created.

---
+++ Assets/Scripts/MeshUtils.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class MeshUtils
+{
+
+	// Separate the given bounds from the worldMatrix
+	// Apply something  similar to marching cubes on it
+	// return the generated Mesh
+	// http://www.cs.carleton.edu/cs_comps/0405/shape/marching_cubes.html
+	public static Mesh CreateSegmentMesh(Vector3[,,] worldMatrix, Bounds segmentBounds)
+	{
+		Mesh mesh = new Mesh();
+
+		
+
+		return mesh;
+	}
+}
